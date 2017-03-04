2b5b8b757a2ac11d838bc848d184f2ceb76b1bbe
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2017-03-04 08:33:09 -0500

    getting busy over here

--- loop.php
+++ loop.php
@@ -15,37 +15,18 @@
 <?php // if there are posts, Start the Loop. ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
+	<div class="eachPost">
+		<?php the_post_thumbnail( 'large' ) ?> 
+		<h2 class="entry-title">
+		        <a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" rel="bookmark">
+		          <?php the_title(); ?>
+		        </a>
+		      </h2>
+	</div>
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<h2 class="entry-title">
-        <a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr(the_title_attribute()); ?>" rel="bookmark">
-          <?php the_title(); ?>
-        </a>
-      </h2>
-
-			<section class="entry-content">
-				<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
-				<?php wp_link_pages( array(
-          'before' => '<div class="page-link"> Pages:',
-          'after' => '</div>'
-        )); ?>
-			</section><!-- .entry-content -->
-
-			<footer>
-				<p><?php the_tags('Tags: ', ', ', '<br>'); ?> Posted in <?php the_category(', '); ?></p>
-        <p><?php comments_popup_link('Respond to this post &raquo;', '1 Response &raquo;', '% Responses &raquo;'); ?></p>
-        <p><?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?></p>
-			</footer>
-
-		</article><!-- #post-## -->
-
-		<?php comments_template( '', true ); ?>
 
 
 <?php endwhile; // End the loop. Whew. ?>
 
 <?php // Display navigation to next/previous pages when applicable ?>
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-  <p class="alignleft"><?php next_posts_link('&laquo; Older Entries'); ?></p>
-  <p class="alignright"><?php previous_posts_link('Newer Entries &raquo;'); ?></p>
-<?php endif; ?>
+
--- single.php
+++ single.php
@@ -2,40 +2,70 @@
 
 <div class="main">
   <div class="container">
-    <div class="content">
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <h1 class="entry-title"><?php the_title(); ?></h1>
 
-          <div class="entry-meta">
-            <?php hackeryou_posted_on(); ?>
-          </div><!-- .entry-meta -->
 
           <div class="entry-content">
-            <?php the_content(); ?>
-            <?php wp_link_pages(array(
-              'before' => '<div class="page-link"> Pages: ',
-              'after' => '</div>'
-            )); ?>
+
+          <!-- video -->
+           <?php while( has_sub_fields('video_projects') ): ?>
+           <div class="Vimeo">
+             
+               <?php the_sub_field('video_styling'); ?>
+               <div class="embed-container">
+                 <?php the_sub_field('embedded_link'); ?>
+               </div>
+               <p class="artistCaption"><?php the_sub_field('video_credits'); ?></p>
+               
+          
+           </div>
+           <?php endwhile;//end of vimeo ?>
+
+            <div class="textContent">
+              <?php the_content(); ?>
+          
+            </div>
+
+            <?php while(has_sub_fields('file_uploader') ): ?>
+            <div class="cvpdf">
+              <?php 
+                  
+                      $file = get_sub_field('the_file');
+                  
+                      if( $file ): ?>
+                        
+                        <a href="<?php echo $file['url']; ?>"><?php echo $file['filename']; ?><?php the_sub_field('hyperlink_text'); ?></a>
+                  
+                      <?php endif; ?>
+            </div>
+          <?php endwhile;//end of CV PDF ?>
+
+            <?php if( have_rows('images') ) : ?>
+                               <ul class="bxslider">
+                                   <?php while( have_rows('images') ) : the_row(); ?>
+                                       <li>
+                                           <?php $image = get_sub_field('image_works');
+                                               if( !empty($image) ): ?>
+                                                   <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                                              
+                                               <?php endif; ?>
+                                       </li>
+                                   <?php endwhile; ?>
+                               </ul><!-- .bxslider -->
+            <?php endif;  // end gallery repeater field?>
+
+
+          
           </div><!-- .entry-content -->
 
-          <div class="entry-utility">
-            <?php hackeryou_posted_in(); ?>
-            <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
-          </div><!-- .entry-utility -->
         </div><!-- #post-## -->
 
-        <div id="nav-below" class="navigation">
-          <p class="nav-previous"><?php previous_post_link('%link', '&larr; %title'); ?></p>
-          <p class="nav-next"><?php next_post_link('%link', '%title &rarr;'); ?></p>
         </div><!-- #nav-below -->
 
-        <?php comments_template( '', true ); ?>
-
       <?php endwhile; // end of the loop. ?>
-
-    </div> <!-- /.content -->
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
