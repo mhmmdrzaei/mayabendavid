81fac0d35e10b43ba7ed61effe655db9dc22dc19
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2017-04-12 17:28:01 -0400

    blreg

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="main">
-  <div class="container">
+  <div class="container singlePost">
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
